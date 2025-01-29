8c03ff5fd1c9940df163dec401d71d6525252ddd
abdel9327 <abdeldjalel93@gmail.com>
2025-01-29 09:24:26 +0100

    add test

--- src/test/java/RagApplicationTests.java
+++ src/test/java/RagApplicationTests.java
@@ -1,10 +1,8 @@
-package fr.efrei.springrag;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringragApplicationTests {
+class RagApplicationTests {
 
 	@Test
 	void contextLoads() {
